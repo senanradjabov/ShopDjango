a705e8efe2d94419b120971196cc3c7eb08b3cc9
ayzek_leasanna <iamtheemperor06@gmail.com>
2023-06-04 22:30:25 +0400

    add get_absolute_url

--- shop/models.py
+++ shop/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 
 class Category(models.Model):
@@ -15,6 +16,11 @@ class Category(models.Model):
     
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse("shop:product_list_by_category", kwargs={'category_slug': self.slug})
+    
+    
     
 
 class Product(models.Model):
@@ -38,3 +44,7 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse("shop:product_detail", kwargs={'id': self.pk, 'slug': self.slug})
+    
