e8577a32f562dfbc4fb3f63ad2e9da7a0d3a2257
ayzek_leasanna <iamtheemperor06@gmail.com>
2023-06-07 10:30:37 +0400

    add urls for cart

--- config/urls.py
+++ config/urls.py
@@ -22,8 +22,10 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('cart/', include('cart.urls', namespace='cart')),
     path('', include('shop.urls', namespace='shop'))
 ]
 
 if settings.DEBUG is True:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
